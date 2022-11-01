db3cf77e9f5c5730d04d556a9853fbc95728d6c7
Ahmed Abd Elfatah <chem.ahmedaf@gmail.com>
2022-11-01 09:50:31 +0200

    fix bug in responsive

--- src/components/ProductDetails/index.jsx
+++ src/components/ProductDetails/index.jsx
@@ -22,33 +22,37 @@ const ProductDetails = () => {
   // console.log(item.rating !== undefined && item.rating.rate);
   return (
     <div className="container">
-      <div className="card text-center w-75 m-auto pt-2 my-3 rounded-5">
-        <figure className="m-0">
-          <img
-            src={item.image}
-            className="card-img-top w-25 m-auto mb-1"
-            alt={item.title}
-          />
-        </figure>
-        <div className="card-body">
-          <h5 className="card-title mb-2 text-secondary fs-3">{item.title}</h5>
-          <p className="category text-success fw-bold fs-4">{item.category}</p>
-          <p className="card-text fw-bolder text-secondary">
-            {item.description}
-          </p>
-          <div className="info d-flex fw-bold text-white fs-4 justify-content-around">
-            <span className="bg-primary py-2 px-3 rounded-4">
-              Price : {item.price} EGP
-            </span>
-            <span className="bg-success py-2 px-3 rounded-4">
-              Quantity : {item.rating !== undefined && item.rating.count}
-            </span>
-            <span className="bg-info py-2 px-3 rounded-4">
-              Rate : {item?.rating?.rate}
-            </span>
-          </div>
-        </div>
-      </div>
+     <div className="row">
+     <div className="offset-md-2 col-md-8">
+     <div className="card text-center m-auto pt-2 my-3 rounded-5">
+     <figure className="m-0">
+       <img
+         src={item.image}
+         className="card-img-top w-25 m-auto mb-1"
+         alt={item.title}
+       />
+     </figure>
+     <div className="card-body">
+       <h5 className="card-title mb-2 text-secondary fs-3">{item.title}</h5>
+       <p className="category text-success fw-bold fs-4">{item.category}</p>
+       <p className="card-text fw-bolder text-secondary">
+         {item.description}
+       </p>
+       <div className="info d-flex fw-bold text-white justify-content-around">
+         <span className="bg-primary py-2 px-3 rounded-4">
+           Price : {item.price} EGP
+         </span>
+         <span className="bg-success py-2 px-3 rounded-4">
+           Quantity : {item.rating !== undefined && item.rating.count}
+         </span>
+         <span className="bg-info py-2 px-3  rounded-4">
+           Rate : {item?.rating?.rate}
+         </span>
+       </div>
+     </div>
+   </div>
+     </div>
+     </div>
     </div>
   );
 };
