313465ae6d5363301a2046b7cfe6afcaeced1849
Ahmed Abd Elfatah <chem.ahmedaf@gmail.com>
2022-11-01 09:17:51 +0200

    add spinner

--- src/components/ProductsList/index.jsx
+++ src/components/ProductsList/index.jsx
@@ -28,40 +28,51 @@ const ProductList = () => {
     <div className="container">
       <div className="Products-list mt-3 row">
         <Fragment>
-          {Products.map((product) => {
-            return (
-              <div className="product col-md-4 mb-5" key={product.id}>
-                <Card className="card rounded-4  text-center py-3 h-100 ">
-                  <Card.Img className=" m-auto w-25 h-75" src={product.image} />
-                  <Card.Body>
-                    <Card.Title className="text-primary fs-4 mb-3">
-                      {product.title}
-                    </Card.Title>
-                    <span className="d-block text-success fw-bold fs-5 mb-3">
-                      {product.price} EGP
-                    </span>
-                    <Link
-                      className="btn btn-primary text-white"
-                      to={`/productDetails/${product.id}`}
-                    >
-                      View Details
-                    </Link>
-                    <button
-                      disabled={
-                        isProductAdded && pdoductIds.includes(product.id)
-                      }
-                      onClick={() => {
-                        addProductTocart(product);
-                      }}
-                      className="btn btn-success ms-3"
-                    >
-                      Add to cart
-                    </button>
-                  </Card.Body>
-                </Card>
+          {!Products.length ? (
+            <div className="d-flex justify-content-center text-primary">
+              <div className="spinner-border" role="status">
+                <span className="visually-hidden">Loading...</span>
               </div>
-            );
-          })}
+            </div>
+          ) : (
+            Products.map((product) => {
+              return (
+                <div className="product col-md-4 mb-5" key={product.id}>
+                  <Card className="card rounded-4  text-center py-3 h-100 ">
+                    <Card.Img
+                      className=" m-auto w-25 h-75"
+                      src={product.image}
+                    />
+                    <Card.Body>
+                      <Card.Title className="text-primary fs-4 mb-3">
+                        {product.title}
+                      </Card.Title>
+                      <span className="d-block text-success fw-bold fs-5 mb-3">
+                        {product.price} EGP
+                      </span>
+                      <Link
+                        className="btn btn-primary text-white"
+                        to={`/productDetails/${product.id}`}
+                      >
+                        View Details
+                      </Link>
+                      <button
+                        disabled={
+                          isProductAdded && pdoductIds.includes(product.id)
+                        }
+                        onClick={() => {
+                          addProductTocart(product);
+                        }}
+                        className="btn btn-success ms-3"
+                      >
+                        Add to cart
+                      </button>
+                    </Card.Body>
+                  </Card>
+                </div>
+              );
+            })
+          )}
         </Fragment>
       </div>
     </div>
